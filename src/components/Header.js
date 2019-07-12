39283dd6e40dc283bf2da5dd83cf568c82984176
Guita Vahdatinia <gvahdatinia@freewheel.tv>
2019-07-12 18:44:13 -0400

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,77 @@
-import React from 'react';
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
-import * as api from '../api';
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import * as api from "../api";
 class Header extends React.Component {
   state = {
     teams: [],
     sprints: [],
-    selected_team: "Tools",
-    selected_sprint: "0"  }
+    selected_team: "Select Team",
+    selected_sprint: "Select Sprint"
+  };
   componentDidMount() {
     api.fetchTeams().then(teams => {
       this.setState({
         teams
-      })
-      api.fetchSprint(this.state.selected_team).then(sprints =>{
+      });
+      api.fetchSprint(this.state.selected_team).then(sprints => {
         this.setState({
           sprints
-        })
-      })
-    })
+        });
+      });
+    });
   }
-  changeTeam = (team) => {
-    api.fetchSprint(team.team).then(sprints =>{
+
+  changeTeam(team){
+    api.fetchSprint(team.team).then(sprints => {
       this.setState({
         sprints: sprints,
-        selected_team: team.team
-      })
-    })
-    this.props.selectedSprint(this.state.sprints);
+        selected_team: team.team,
+        selected_sprint: sprints[0]
+      });
+      this.props.selectedSprint(sprints[0]);
+    });
     this.props.selectedTeam(team.team);
-  }
-  changeSprint(sprint){
+  };
+  changeSprint(sprint) {
     this.setState({
       selected_sprint: sprint.sprint
-    })
+    });
     this.props.selectedSprint(sprint.sprint);
   }
   render() {
     return (
-    <div>
-      <Navbar>
-        <Navbar.Brand href="#home" className="title">
-          Retro
-        </Navbar.Brand>
-        <NavDropdown title={this.state.selected_team} id="basic-nav-dropdown">
-          {this.state.teams.map(team=>
-            <NavDropdown.Item href="#" onClick={this.changeTeam.bind(this, {team})}>
-              {team}</NavDropdown.Item>)}
-        </NavDropdown>
-        <NavDropdown title={this.state.selected_sprint} id="basic-nav-dropdown">
-          {this.state.sprints.map(sprint=>
-            <NavDropdown.Item href="#" onClick={this.changeSprint.bind(this, {sprint})}>
-              {sprint}</NavDropdown.Item>)}
-        </NavDropdown>
-      </Navbar>
-    </div>
-  );
+      <div>
+        <Navbar>
+          <Navbar.Brand href="#home" className="title">
+            Retro
+          </Navbar.Brand>
+          <NavDropdown title={this.state.selected_team} id="basic-nav-dropdown">
+            {this.state.teams.map(team => (
+              <NavDropdown.Item
+                href="#"
+                onClick={this.changeTeam.bind(this, { team })}
+              >
+                {team}
+              </NavDropdown.Item>
+            ))}
+          </NavDropdown>
+          <NavDropdown
+            title={this.state.selected_sprint}
+            id="basic-nav-dropdown"
+          >
+            {this.state.sprints.map(sprint => (
+              <NavDropdown.Item
+                href="#"
+                onClick={this.changeSprint.bind(this, { sprint })}
+              >
+                {sprint}
+              </NavDropdown.Item>
+            ))}
+          </NavDropdown>
+        </Navbar>
+      </div>
+    );
   }
-};
-
+}
 
 export default Header;
