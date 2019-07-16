a5a746ab2983e813011fb9a05ebbe4fe72fbb4f9
Guita Vahdatinia <gvahdatinia@freewheel.tv>
2019-07-16 03:19:09 -0400

    Fix empty lists in headboard

--- src/components/TeamBoard.js
+++ src/components/TeamBoard.js
@@ -5,14 +5,13 @@ import Popup from "./Popup";
 import * as api from "../api";
 import equal from "fast-deep-equal";
 import Category from "./Category";
-import Header from "./Header"
-import Splash from "./Splash"
+import Header from "./Header";
+import Splash from "./Splash";
 
-const pushState = (obj, url) =>
-  window.history.pushState(obj, '', url);
+const pushState = (obj, url) => window.history.pushState(obj, "", url);
 
 class TeamBoard extends React.Component {
-  state = this.props.initialData
+  state = this.props.initialData;
 
   getDataFromChild = (cat, data) => {
     this.setState({ welldata: data });
@@ -35,13 +34,12 @@ class TeamBoard extends React.Component {
     });
   }
   callIncrease = (desc, color) => {
-    api.upVote(this.state.team, this.state.sprint, color, desc.item)
-    .then(resp =>
-    this.fetchingLists())
-  }
+    api
+      .upVote(this.state.team, this.state.sprint, color, desc.item)
+      .then(resp => this.fetchingLists());
+  };
   fetchingLists() {
-    api.fetchItems(this.state.team, this.state.sprint)
-    .then(items => {
+    api.fetchItems(this.state.team, this.state.sprint).then(items => {
       this.setState({
         bad: items.bad,
         well: items.well,
@@ -50,7 +48,8 @@ class TeamBoard extends React.Component {
     });
   }
   componentDidUpdate(prevProps, prevState) {
-    if (!equal (this.state.sprint, "")){}
+    if (!equal(this.state.sprint, "")) {
+    }
     if (!equal(this.state.welldata, prevState.welldata)) {
       api
         .postDescription(
@@ -61,108 +60,122 @@ class TeamBoard extends React.Component {
         )
         .then(value => this.fetchingLists());
     }
-    if (!equal(this.state.sprint, prevState.sprint) )
-      {this.fetchingLists()}
+    if (!equal(this.state.sprint, prevState.sprint)) {
+      this.fetchingLists();
     }
-  onselectTeam = (team) => {
-    this.setState({ team: team})
   }
-  onselectSprint = (sprint) =>{
-    this.setState({ sprint: sprint})
+  onselectTeam = team => {
+    this.setState({ team: team });
+  };
+  onselectSprint = sprint => {
+    this.setState({ sprint: sprint });
+    pushState({ selectSprint: sprint }, `/${this.state.team}/${sprint}`);
+  };
+  selectSplash = (team, sprint) => {
+    this.setState({ team: team, sprint: sprint });
     pushState(
-      {selectSprint: sprint},
-      `/${this.state.team}/${sprint}`
-    )
-  }
-  selectSplash = (team, sprint) =>{
-    this.setState({  team: team, sprint: sprint})
-    pushState(
-      { selectedTeam: team, selectSprint: sprint},
+      { selectedTeam: team, selectSprint: sprint },
       `/${team}/${sprint}`
-    )
+    );
+  };
+  checkCategoryEmpty(category){
+    if(Object.keys(this.state[category]).length !== 0){
+      return (
+        <Card.Body className={`card_body ${category}`}>
+          <Category
+            items={this.state[category]}
+            color={`${category}`}
+            upvoted={this.callIncrease.bind(this)}
+          />
+        </Card.Body>
+      )
+    }
+    else {
+      return (
+      <h1>Click on the cateogry to add a card</h1>)
+    }
   }
   currentContent() {
-  if (this.state.sprint) {
-    return (<div>
-    <Header selectedTeam={this.onselectTeam} selectedSprint={this.onselectSprint} team={this.state.team} sprint={this.state.sprint}/>
-    <Container fluid={true}>
-      <Row>
-        <Col s={12} md={4}>
-          <Card>
-            {this.state.showWellPopup ? (
-              <Popup
-                text='Click "Close Button" to hide popup'
-                closePopup={this.toggleWellPopup.bind(this)}
-                category={"well"}
-                sendData={this.getDataFromChild}
-              />
-            ) : null}
-            <Card.Body
-              className="card_title"
-              onClick={this.toggleWellPopup.bind(this)}
-            >
-              Went Well
-            </Card.Body>
-          </Card>
-          <Card.Body className={'card_body well'}>
-          <Category items={this.state.well} color={"well"} upvoted={this.callIncrease.bind(this)}/>
-          </Card.Body>
+    if (this.state.sprint) {
+      return (
+        <div>
+          <Header
+            selectedTeam={this.onselectTeam}
+            selectedSprint={this.onselectSprint}
+            team={this.state.team}
+            sprint={this.state.sprint}
+          />
+          <Container fluid={true}>
+            <Row>
+              <Col s={12} md={4}>
+                <Card>
+                  {this.state.showWellPopup ? (
+                    <Popup
+                      text='Click "Close Button" to hide popup'
+                      closePopup={this.toggleWellPopup.bind(this)}
+                      category={"well"}
+                      sendData={this.getDataFromChild}
+                    />
+                  ) : null}
+                  <Card.Body
+                    className="card_title"
+                    onClick={this.toggleWellPopup.bind(this)}
+                  >
+                    Went Well
+                  </Card.Body>
+                </Card>
+                {this.checkCategoryEmpty("well")}
+              </Col>
+              <Col s={12} md={4}>
+                <Card>
+                  {this.state.showBadPopup ? (
+                    <Popup
+                      text='Click "Close Button" to hide popup'
+                      closePopup={this.toggleBadPopup.bind(this)}
+                      category={"bad"}
+                      sendData={this.getDataFromChild}
+                    />
+                  ) : null}
+                  <Card.Body
+                    className="card_title"
+                    onClick={this.toggleBadPopup.bind(this)}
+                  >
+                    Improve On
+                  </Card.Body>
+                </Card>
+                {this.checkCategoryEmpty("bad")}
+              </Col>
+              <Col s={12} md={4}>
+                <Card>
+                  {this.state.showImprovePopup ? (
+                    <Popup
+                      text='Click "Close Button" to hide popup'
+                      closePopup={this.toggleImprovePopup.bind(this)}
+                      category={"todo"}
+                      sendData={this.getDataFromChild}
+                    />
+                  ) : null}
+                  <Card.Body
+                    className="card_title"
+                    onClick={this.toggleImprovePopup.bind(this)}
+                  >
+                    To Do
+                  </Card.Body>
+                </Card>
+                {this.checkCategoryEmpty("todo")}
+              </Col>
+            </Row>
+          </Container>{" "}
+        </div>
+      );
+    }
 
-      </Col>
-        <Col s={12} md={4}>
-          <Card>
-            {this.state.showBadPopup ? (
-              <Popup
-                text='Click "Close Button" to hide popup'
-                closePopup={this.toggleBadPopup.bind(this)}
-                category={"bad"}
-                sendData={this.getDataFromChild}
-              />
-            ) : null}
-            <Card.Body
-              className="card_title"
-              onClick={this.toggleBadPopup.bind(this)}
-            >
-              Improve On
-            </Card.Body>
-          </Card>
-          <Card.Body className={'card_body bad'}>
-          <Category items={this.state.bad} color={"bad"} upvoted={this.callIncrease.bind(this)}/>
-          </Card.Body>
-      </Col>
-        <Col s={12} md={4}>
-          <Card>
-            {this.state.showImprovePopup ? (
-              <Popup
-                text='Click "Close Button" to hide popup'
-                closePopup={this.toggleImprovePopup.bind(this)}
-                category={"todo"}
-                sendData={this.getDataFromChild}
-              />
-            ) : null}
-            <Card.Body
-              className="card_title"
-              onClick={this.toggleImprovePopup.bind(this)}
-            >
-              To Do
-            </Card.Body>
-          </Card>
-          <Card.Body className={'card_body todo'}>
-          <Category items={this.state.todo} color={"todo"} upvoted={this.callIncrease.bind(this)}/>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Container> </div>)
-  }
-
-  return <Splash teams={this.state.teams} selectedSprint={this.selectSplash}/>;
+    return (
+      <Splash teams={this.state.teams} selectedSprint={this.selectSplash} />
+    );
   }
   render() {
-    return (
-      <div>
-        {this.currentContent()}
-      </div>
-    );
+    return <div>{this.currentContent()}</div>;
   }
 }
 
